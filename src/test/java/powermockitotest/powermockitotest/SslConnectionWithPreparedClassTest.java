4ea97fa5892c2c66659d87ea011b4aca534ef53c
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2010-10-30 21:35:25 +0200

    Include a test which uses powermockito.

--- src/test/java/powermockitotest/powermockitotest/SslConnectionWithPreparedClassTest.java
+++ src/test/java/powermockitotest/powermockitotest/SslConnectionWithPreparedClassTest.java
@@ -1,13 +1,17 @@
 package powermockitotest.powermockitotest;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -15,19 +19,27 @@ import org.powermock.modules.junit4.PowerMockRunner;
  * Unit test for simple App.
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ClassWithFinalMethods.class})
-public class SslConnectionWithPreparedClassTest 
-{
+@PrepareForTest({ ClassWithFinalMethods.class })
+public class SslConnectionWithPreparedClassTest {
+
+    @Test
+    public void testMock() {
+        final ClassWithFinalMethods mock = PowerMockito.mock(ClassWithFinalMethods.class);
+        PowerMockito.doReturn("Hullo").when(mock).getString();
+        assertEquals("Hullo", mock.getString());
+    }
+
     /**
      * Rigourous Test :-)
-     * @throws IOException 
-     * @throws ClientProtocolException 
+     * 
+     * @throws IOException
+     * @throws ClientProtocolException
      */
     @Test
-    public void testApp() throws ClientProtocolException, IOException    
-    {
+    public void testSslConnection() throws ClientProtocolException, IOException {
         final HttpClient client = new DefaultHttpClient();
         final HttpGet get = new HttpGet("https://www.gmx.net/");
-        client.execute(get);
+        final String html = client.execute(get, new BasicResponseHandler());
+        System.out.println(html);
     }
 }
